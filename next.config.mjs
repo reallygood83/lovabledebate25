10a0a4cf4d6ab2b9f3734daefdaf8ab971fded41
Moon-Jung <usmjkim23@gmail.com>
2025-04-24 20:53:04 +0900

    최적화: 서버리스 함수 크기 제한 해결 및 .gitignore 업데이트

--- next.config.mjs
+++ next.config.mjs
@@ -34,8 +34,48 @@ const nextConfig = {
   // Next.js 15.3.1에 맞게 업데이트된 설정
   // 실험적 기능 대신 루트 레벨 설정 사용
   serverExternalPackages: ['bcrypt'],
+  
+  // 서버리스 함수 크기 최적화 설정
   outputFileTracingIncludes: {
     '/**/*': ['./node_modules/**/*']
+  },
+  
+  // 불필요한 파일을 트레이싱에서 제외
+  outputFileTracingExcludes: {
+    '/**/*': [
+      './node_modules/@next/swc-linux-x64-gnu/**/*',
+      './node_modules/@next/swc-linux-x64-musl/**/*',
+      // react-icons에서 사용하지 않는 모든 패키지 제외 (fa만 유지)
+      './node_modules/react-icons/gi/**/*',
+      './node_modules/react-icons/pi/**/*',
+      './node_modules/react-icons/si/**/*',
+      './node_modules/react-icons/tb/**/*',
+      './node_modules/react-icons/md/**/*',
+      './node_modules/react-icons/lia/**/*',
+      './node_modules/react-icons/ri/**/*',
+      './node_modules/react-icons/bs/**/*',
+      './node_modules/react-icons/ai/**/*',
+      './node_modules/react-icons/bi/**/*',
+      './node_modules/react-icons/ci/**/*',
+      './node_modules/react-icons/di/**/*',
+      './node_modules/react-icons/fi/**/*',
+      './node_modules/react-icons/fc/**/*',
+      './node_modules/react-icons/go/**/*',
+      './node_modules/react-icons/gr/**/*',
+      './node_modules/react-icons/hi/**/*',
+      './node_modules/react-icons/im/**/*',
+      './node_modules/react-icons/io/**/*',
+      './node_modules/react-icons/lu/**/*',
+      './node_modules/react-icons/rx/**/*',
+      './node_modules/react-icons/sl/**/*',
+      './node_modules/react-icons/ti/**/*',
+      './node_modules/react-icons/vsc/**/*',
+      './node_modules/react-icons/wi/**/*',
+      './node_modules/typescript/**/*',
+      './node_modules/@img/sharp-libvips-linux*/**/*',
+      './node_modules/@img/sharp-libvips-linuxmusl*/**/*',
+      './node_modules/eslint/**/*'
+    ]
   }
 };
 
